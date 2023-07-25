0dee05af5767467811801c5e4bccf5178ecaca2f
nitin8971 <121596217+nitin8971@users.noreply.github.com>
2023-07-25 21:06:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/search-and-scrape', async (req, res) => {
+    const { query } = req.body;
+    try {
+        const searchResults = await getSearchResults(query);
+        const scrapedText = await scrapeURLs(searchResults);
+        res.json({ results: scrapedText });
+    } catch (error) {
+        console.error('Error during search and scraping:', error.message);
+        res.status(500).json({ error: 'Error during search and scraping' });
+    }
+});
+
+async function getSearchResults(query) {
+    const apiKey = 'AIzaSyDpUQxQMUyyJ4TY-VR58fMtRt9b9nOzdsk'; // Replace with your Custom Search API key
+    const cx = '7d594fa13e424201';   // Replace with your Custom Search Engine ID
+    const url = `https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(query)}&key=${apiKey}&cx=${cx}&num=5`;
+
+    try {
+        const response = await axios.get(url);
+        return response.data.items.map(item => item.link);
+    } catch (error) {
+        throw new Error('Error fetching search results from Custom Search API');
+    }
+}
+
+async function scrapeURLs(searchResults) {
+    const scrapingBeeAPIKey = 'HJFMXMI6JC69V005TLVZY82GE3QQYGSSAQMW80E2S0CIPAHMN6NT50XI05KZPG9HN6L9QCR59BLUXT15'; // Replace with your ScrapingBee API key
+    const scrapedTexts = [];
+
+    for (const url of searchResults) {
+        const scrapingUrl = `https://app.scrapingbee.com/api/v1/extract?url=${encodeURIComponent(url)}&api_key=${scrapingBeeAPIKey}`;
+
+        try {
+            const response = await axios.get(scrapingUrl);
+            scrapedTexts.push(response.data.text);
+        } catch (error) {
+            console.error(`Error scraping ${url} with ScrapingBee API:`, error.message);
+            scrapedTexts.push('Error scraping the URL.');
+        }
+    }
+
+    return scrapedTexts;
+}
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ main.js
@@ -0,0 +1,34 @@
+function searchAndScrape() {
+    const searchInput = document.getElementById('searchInput').value;
+    if (!searchInput) {
+        alert('Please enter a search query.');
+        return;
+    }
+
+    fetch('/search-and-scrape', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ query: searchInput })
+    })
+    .then(response => response.json())
+    .then(data => {
+        displayResults(data.results);
+    })
+    .catch(error => {
+        console.error('Error during search and scraping:', error.message);
+        alert('Error during search and scraping. Please try again later.');
+    });
+}
+
+function displayResults(results) {
+    const resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = ''; // Clear previous results
+
+    results.forEach((text, index) => {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = `Result ${index + 1}: ${text}`;
+        resultsDiv.appendChild(paragraph);
+    });
+}
